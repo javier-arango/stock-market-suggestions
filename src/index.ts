413651400f86f49877861d2a6658ca61e42be0c9
Eddie Lopez <ejlopez00@gmail.com>
2022-04-11 16:04:47 -0400

    foundational: first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 5000;
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/types/data.ts
@@ -0,0 +1,17 @@
+/** Datum object from "API" */
+export type Datum = {
+    date: Date;
+    price: number;
+    dcf: number;
+};
+
+/** Extracted stock information from Data */
+export type Stock = {
+    investmentRating: number;
+    ticker: string;
+    name: string;
+    data: Array<Datum>; // sorted by date
+}
+
+/** Processed data to be delivered to Frontend */
+export type Stocks = Array<Stock>;
