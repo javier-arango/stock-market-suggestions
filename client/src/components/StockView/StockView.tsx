ae20ab697a7a9d81e60ef94de3bda09415257178
Jean <fonrosejeangregor@gmail.com>
2022-04-18 11:16:07 -0400

    pass down the marketInfo props to MarketInfo component

--- client/src/components/StockView/StockView.tsx
+++ client/src/components/StockView/StockView.tsx
@@ -3,14 +3,14 @@ import StockMarketInfo from '../StockMarketInfo/StockMarketInfo';
 import StockData from '../StockData/StockData';
 import './stockview.css';
 
-function StockView() {
+function StockView(props: any) {
 
-  return (
-    <div id="stock-view" className="col d-flex flex-column align-items-center justify-content-center">
-      <StockMarketInfo />
-      <StockData />
-    </div>
-  );
+    return (
+        <div id="stock-view" className="col d-flex flex-column align-items-center justify-content-center">
+        <StockMarketInfo marketInfo={props.marketInfo}/>
+        <StockData data={props.stock} />
+        </div>
+    );
 }
 
 export default StockView;
