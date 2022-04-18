6185ed239dac3c74f6ed60fabc00c599bf231283
Jean <fonrosejeangregor@gmail.com>
2022-04-18 11:13:16 -0400

    Added empty view when no stock is selected and also program time frame to select when displaying the stock graph

--- client/src/components/StockData/StockData.tsx
+++ client/src/components/StockData/StockData.tsx
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-function StockData() {
+function StockData(props: any) {
+  const [currentTime, setCurrentTime] = useState(1);
+  const timeFrame = ["1M", "3M", "6M", "1YR", "5YR"];
+
+  const changeTime = (index: number) => {
+    setCurrentTime(() => {
+      return index;
+    });
+  };
 
   return (
     <div className="row w-100 h-100 pb-5 mt-4 d-flex justify-content-center">
-        <div className="col-10 chart-info w-80 rounded">
-          <div className="d-flex flex-row justify-content-between border-bottom p-2">
-            <div className="">
-              <h5 className="sticker">AAPL</h5>
-              <small className="name">Apple</small>
-            </div>
-            <div className="">
-              <h5 className="rating">DCF</h5>
-              <small className="recom">Apple</small>
-            </div>
+      <div className="col-10 chart-info w-80 rounded">
+        {Object.keys(props.data).length === 0 ? (
+          <div className="w-100 h-100 d-flex justify-content-center align-items-center no-stock-selected">
+            Select a stock from the list
           </div>
-          <div className=""></div>
-          <div className="d-flex flex-row justify-content-end border-top p-2">
-            <small className="t-frame t-active">1W</small>
-            <small className="t-frame">1M</small>
-            <small className="t-frame">3M</small>
-            <small className="t-frame">6M</small>
-            <small className="t-frame">1YR</small>
-          </div>
-        </div>
+        ) : (
+          <>
+            <div className="d-flex flex-row justify-content-between border-bottom p-2">
+              <div className="">
+                <h5 className="sticker">{props.data.sticker}</h5>
+                <small className="name">{props.data.name}</small>
+              </div>
+              <div className="">
+                <h5 className="rating">Investment Rating</h5>
+                <small className="recom">Apple</small>
+              </div>
+            </div>
+            <div className=""></div>
+            <div className="d-flex flex-row justify-content-end border-top p-2">
+              {timeFrame.map((time: string, index) => {
+                const isActive = index == currentTime ? "t-active" : "";
+                return (
+                  <small
+                    key={index}
+                    className={`t-frame ${isActive}`}
+                    onClick={() => changeTime(index)}
+                  >
+                    {time}
+                  </small>
+                );
+              })}
+            </div>
+          </>
+        )}
       </div>
+    </div>
   );
 }
 
