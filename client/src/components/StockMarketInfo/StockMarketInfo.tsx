05137f6966e24c13dcaa3f884d0b8c20b06b281b
Jean <fonrosejeangregor@gmail.com>
2022-04-18 11:14:54 -0400

    Made random data for the 3 major stock indices and made state in App.tsx so it doesn't give a new number when user select a stock

--- client/src/components/StockMarketInfo/StockMarketInfo.tsx
+++ client/src/components/StockMarketInfo/StockMarketInfo.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function StockMarketInfo() {
-
+function StockMarketInfo(props: any) {
   return (
     <div className="row w-100 mt-5 d-flex justify-content-around">
-        <div className="col-2 market-info d-flex flex-column align-items-center pt-2 rounded-3">
-          <h6>DOW JONES</h6>
-          <p className="num">3456.00</p>
-          <p className="p-green">+1.75%</p>
-        </div>
-        <div className="col-2 market-info d-flex flex-column align-items-center pt-2 rounded">
-          <h6>NASDAQ</h6>
-          <p className="num">3456.00</p>
-          <p className="p-red">+1.75%</p>
-        </div>
-        <div className="col-2 market-info d-flex flex-column align-items-center pt-2 rounded">
-          <h6>S&P 500</h6>
-          <p className="num">3456.00</p>
-          <p className="p-green">+1.75%</p>
-        </div>
-      </div>
+      {props.marketInfo.map((market: any) => {
+        const color = Number(market.percentage) < 0 ? "p-red" : "p-green";
+
+        return (
+          <div
+            key={market.name}
+            className="col-2 market-info d-flex flex-column align-items-center pt-2 rounded-3"
+          >
+            <h6>{market.name}</h6>
+            <p className="num">{market.index}</p>
+            <p className={color}>{market.percentage}%</p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
