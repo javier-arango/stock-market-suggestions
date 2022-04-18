7e8c1f18a7de092fa1eaaf8cd151fa3412173716
Jean <fonrosejeangregor@gmail.com>
2022-04-18 14:26:11 -0400

    Change design of the sorting component

--- client/src/components/SortSelection/SortSelection.tsx
+++ client/src/components/SortSelection/SortSelection.tsx
@@ -2,29 +2,31 @@ import React from "react";
 
 function SideBar(props: any) {
   return (
-    <div className="w-100 h-20 d-flex ms-3 my-5">
-      <p className="d-flex align-items-center">Sort Algorithm:</p>
-      {props.algorithms.map((algorithm: string) => {
-        return (
-          <div key={algorithm}>
-            <input
-              type="radio"
-              className="btn-check"
-              name="options-outlined"
-              id={algorithm}
-              autoComplete="off"
-              checked={props.selAlgo == algorithm ? true : false}
-              onChange={() => props.changeAlgorithm(algorithm)}
-            />
-            <label
-              className="btn btn-outline-secondary ms-3"
-              htmlFor={algorithm}
-            >
-              {algorithm}
-            </label>
-          </div>
-        );
-      })}
+    <div className="w-100 h-20 d-flex flex-column my-5 justify-content-center align-items-center">
+      <p className="d-flex align-items-center">Sort Algorithm</p>
+      <div className="d-flex">
+        {props.algorithms.map((algorithm: string) => {
+          return (
+            <div key={algorithm}>
+              <input
+                type="radio"
+                className="btn-check"
+                name="options-outlined"
+                id={algorithm}
+                autoComplete="off"
+                checked={props.selAlgo == algorithm ? true : false}
+                onChange={() => props.changeAlgorithm(algorithm)}
+              />
+              <label
+                className="btn btn-outline-secondary ms-3"
+                htmlFor={algorithm}
+              >
+                {algorithm}
+              </label>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
